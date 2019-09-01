1f6c12e6623e66b2a222a26eddfff92b3f25d13a
Kevin Arias <kevinlh1337@gmail.com>
2019-09-01 13:01:48 -0500

    Se sube el proyecto a github/rama remota.

---
+++ test2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double horastrabajadas = double.Parse(txthorastrabajadas.Text);
+            double valorhora = double.Parse(txtvalorhora.Text);
+            double total = horastrabajadas * valorhora;
+
+            if (total>750000) {
+                MessageBox.Show("El empleado gana más del salario mínimo");         
+            }
+            else {          
+                total = (total * 0.20)+ total;
+            }
+            lbltotal.Text = total.ToString();
+            
+            //lbltotal.Text = "$" + Convert.ToDecimal(total).ToString(); Convertir a decimal el salario final (Opcional).
+            
+        }
+    }
+}
